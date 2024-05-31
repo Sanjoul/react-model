55e4fd192b7d71d87e089ae996b75020fb0ea8ce
Sanjoul Bhusal <bsanjoul@gmail.com>
2024-05-31 10:59:01 +0545

    React(Popup)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>React popups</h1>
+        <br />
+        <br />
+        <button>Open Popup</button>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
